2389693dccaa6565ba1784cd2ac16c9296ffa38a
6ppng <hiroshi@6ppng-Surface.localdomain>
2020-10-09 02:44:06 +0900

    [fix] 微修正

--- test.py
+++ test.py
@@ -1,36 +1,38 @@
 from requests_oauthlib import OAuth1Session
 import yaml
 
-KEYS_RATH: str = './downloads/config.yaml'
+KEYS_RATH: str = './config/config.yaml'
+URL = "https://api.twitter.com/1.1/statuses/update.json"
 
 
-def get_config(path: str) -> dict:
+def get_config() -> dict:
     with open(KEYS_RATH) as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
     return config
 
 
 def get_keys() -> dict:
-    return get_config(KEYS_RATH)['keys']
+    return get_config()['keys']
 
 
 def post(tweet: str) -> int:
     keys = get_keys()
-    url = "https://api.twitter.com/1.1/statuses/update.json"
     params = {"status": tweet}
-    twitter = OAuth1Session(
-        keys['CK'],
-        keys['CS'],
-        keys['AT'],
-        keys['AS'])
-
-    req = twitter.post(url, params=params)
+    with OAuth1Session(
+            keys['CK'],
+            keys['CS'],
+            keys['AT'],
+            keys['AS']) as twitter:
+        req = twitter.post(URL, params=params)
 
     return req.status_code
 
 
 def main() -> None:
-    post('neyo')
+    res = post('test')
+
+    if res != 200:
+        print(f'error : {res}')
 
 
 if __name__ == '__main__':
