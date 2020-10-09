71add812c36558f6da9d8a4fb5fd13c133bf3d43
6ppng <hiroshi@6ppng-Surface.localdomain>
2020-10-09 23:55:36 +0900

    [fix]

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from tzlocal import get_localzone
 import json
 from requests_oauthlib import OAuth1Session
@@ -29,38 +29,73 @@ def post(tweet: str) -> None:
         print(f'error : {req.status_code}')
 
 
-def get_resource(timeline: str) -> str:
-    if timeline == 'home':
+def get_resource(timeline_name: str) -> str:
+    if timeline_name == 'home':
         return 'https://api.twitter.com/1.1/statuses/home_timeline.json'
-    list_id = get_config()['list_ids'][timeline]
+    list_id = get_config()['list_ids'][timeline_name]
     return 'https://api.twitter.com/1.1/lists/statuses.json' + \
         f'?list_id={list_id}'
 
 
-def reload_timeline(timeline: str = 'home') -> None:
+def get_timeline(timeline_name: str) -> list:
+
     keys = get_keys()
-    url = get_resource(timeline)
+    url = get_resource(timeline_name)
     params = {'counts': 100}
     with OAuth1Session(
             keys['CK'], keys['CS'], keys['AT'], keys['AS']) as twitter:
         req = twitter.get(url, params=params)
 
     if req.status_code == 200:
-        timeline = json.loads(req.text)
-        for tweet in timeline[::-1]:
-            time = datetime.strptime(
-                tweet['created_at'], '%a %b %d %I:%M:%S %z %Y')
-            time = time.astimezone(tz=get_localzone())
-            print(tweet['user']['screen_name'], end=' :: ')
-            print(time.strftime('%H:%M:%S'))
-            print(tweet['text'])
-            print('___')
-    else:
-        print(f'error : {req.status_code}')
+        return json.loads(req.text)
+
+    print(f'error : {req.status_code}')
+    return None
+
+
+def draw_timeline(timeline: list) -> None:
+
+    jp = get_localzone()
+    now = datetime.now().astimezone(tz=jp)
+
+    for tweet in timeline[::-1]:
+
+        time = datetime.strptime(
+            tweet['created_at'], '%a %b %d %H:%M:%S %z %Y').astimezone(tz=jp)
+
+        pass_time = now - time
+        if pass_time < timedelta(hours=1):
+            time_view = time.strftime('%M:%S')
+        elif pass_time < timedelta(days=1):
+            time_view = time.strftime('%H:%M:%S')
+        else:
+            time_view = time.strftime('%m/%d %H:%M:%S')
+
+        if tweet['text'][:2] == 'RT':
+            fav = tweet['retweeted_status']['favorite_count']
+        else:
+            fav = tweet['favorite_count']
+
+        retweet = tweet["retweet_count"]
+
+        print(tweet['user']['screen_name'][:5], end=' ')
+        print(time_view, end=' : ')
+        print(tweet['text'].replace('\n', ' '), end='')
+        if fav != 0:
+            print(f' fav:{fav}', end='')
+        if retweet != 0:
+            print(f' RT:{retweet}', end='')
+        print()
+
+
+def reload(timeline_name: str = 'home') -> None:
+    timeline = get_timeline(timeline_name)
+    if timeline is not None:
+        draw_timeline(timeline)
 
 
 def main() -> None:
-    reload_timeline()
+    reload('friends')
 
 
 if __name__ == '__main__':
